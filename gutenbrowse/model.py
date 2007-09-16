4cdd9dde89053afff1f188118c0e5c0d5275404c
Pauli Virtanen <pav@iki.fi>
2007-09-16 14:03:37 +0300

    Implement downloading, clean up ebook list filling

--- gutenbrowse/model.py
+++ gutenbrowse/model.py
@@ -1,4 +1,4 @@
-import re, os
+import re, os, sys, shutil, urllib
 import gtk
 import gutenbergweb
 
@@ -37,7 +37,8 @@ class EbookList(gtk.ListStore):
     """
     List of ebooks:
 
-        [(author, title, language, file_name), ...]
+        [(author, '', '',
+            [(title, language, file_name), ...]), ...]
     """
     
     def __init__(self, base_directory):
@@ -55,8 +56,7 @@ class EbookList(gtk.ListStore):
                 full_path = os.path.join(d, path)
                 if os.path.isdir(full_path):
                     # recurse into a directory
-                    was_author = (',' in author_name or
-                                  _is_caps_case(author_name))
+                    was_author = (',' in author_name)
                     if not author_name or not was_author:
                         walk_tree(files, full_path, path)
                     else:
@@ -82,17 +82,11 @@ class EbookList(gtk.ListStore):
                     files.append(entry)
 
         def really_add(r):
-            # Inserting stuff to GtkTreeView goes slowly on Maemo,
-            # so we slow down the rate a bit. This makes the app usable
-            # during insertion...
-            for x in r[:100]:
+            for x in r:
                 self.append(x)
-            del r[:100]
-            if r:
-                run_later_in_gui_thread(200, really_add, r)
-            else:
+            if callback:
                 callback()
-
+        
         def do_walk_tree(d):
             files = []
             walk_tree(files, self.base_directory)
@@ -102,24 +96,25 @@ class EbookList(gtk.ListStore):
         start_thread(do_walk_tree, self.base_directory)
 
 
-NEXT_ID = -1
-PREV_ID = -2
+NEXT_ID = -10
+PREV_ID = -20
 
 class GutenbergSearchList(gtk.ListStore):
     """
     List of search results:
 
-        [(author, title, language, etext_id), ...]
+        [(author, title, language, category, etext_id), ...]
     """
     
     def __init__(self):
-        gtk.ListStore.__init__(self, str, str, str, int)
+        gtk.ListStore.__init__(self, str, str, str, str, int)
         self.pageno = 0
         self.max_pageno = None
         self.last_search = None
         
-    def add(self, author=u"", title=u"", language=u"", etext_id=-1):
-        self.append((author, title, language, etext_id))
+    def add(self, author=u"", title=u"", language=u"",
+            category=u"", etext_id=-1):
+        self.append((author, title, language, category, etext_id))
         
     def new_search(self, author="", title="", callback=None):
         self.last_search = (author, title)
@@ -173,23 +168,26 @@ class GutenbergSearchList(gtk.ListStore):
         self.clear()
 
         if self.pageno > 0:
-            self.add(_('(Previous...)'), '', '', PREV_ID)
+            self.add(_('(Previous...)'), '', '', '', PREV_ID)
 
         for x in r:
-            self.add(x[1], x[2], x[3], x[0])
+            self.add(x[1], x[2], x[3], x[4], x[0])
             
         if self.max_pageno is None or self.pageno < self.max_pageno:
-            self.add(_('(Next...)'), '', '', NEXT_ID)
+            self.add(_('(Next...)'), '', '', '', NEXT_ID)
         
     def get_downloads(self, it, callback=None):
-        author, title, language, etext_id = self[it]
-        info = DownloadInfo(author, title, language, etext_id)
+        author, title, language, category, etext_id = self[it]
+        info = DownloadInfo(author, title, language, category, etext_id)
 
-        def on_finish(r):
+        def on_finish(result):
+            r, infodict = result
+            info.category = infodict['category']
             for url, format, encoding, compression in r:
-                info.add(url, ', '.join(format, encoding, compression))
+                msg = [x for x in format, encoding, compression if x]
+                info.add(url, ', '.join(msg))
             if callback:
-                callback(info)
+                callback()
 
         run_in_background(gutenbergweb.etext_info, etext_id,
                           callback=on_finish)
@@ -202,13 +200,97 @@ class DownloadInfo(gtk.ListStore):
 
         [(url, format info)]
     """
-    def __init__(self, author, title, language, etext_id):
+    def __init__(self, author, title, language, category, etext_id):
         self.author = author
         self.title = title
         self.language = language
+        self.category = category
         self.etext_id = etext_id
 
         gtk.ListStore.__init__(self, str, str)
 
     def add(self, url, format_info):
         self.append((url, format_info))
+
+    def download(self, it, base_directory, overwrite=False, callback=None):
+        url, format = self[it]
+
+        author = clean_filename(clean_author(self.author))
+
+        url_base = url.split('/')[-1]
+        try:
+            ext = url_base.split('.', 1)[1]
+        except IndexError:
+            ext = ''
+
+        if self.author and self.title and self.language:
+            base_name = u"%s - %s [%s]" % (author,
+                                           self.title,
+                                           self.language.lower())
+        elif self.author and self.title:
+            base_name = u"%s - %s" % (author, self.title)
+        elif self.title:
+            base_name = u"%s" % self.title
+        else:
+            base_name = u"Etext %d" % self.etext_id
+        
+        if ext:
+            if get_valid_basename(url_base) is None:
+                # Download audio files w/o renaming
+                file_name = clean_filename(url_base)
+            else:
+                file_name = clean_filename("%s.%s" % (base_name, ext))
+        else:
+            file_name = clean_filename(base_name)
+
+        if author:
+            path = os.path.join(base_directory, author, file_name)
+        else:
+            path = os.path.join(base_directory, file_name)
+        dir_path = os.path.dirname(path)
+
+        if not os.path.isdir(dir_path):
+            os.makedirs(dir_path)
+
+        if os.path.isfile(path) and not overwrite:
+            return False
+        
+        def do_download():
+            h = urllib.urlopen(url)
+            f = open(path, 'w')
+            try:
+                shutil.copyfileobj(h, f)
+            finally:
+                h.close()
+                f.close()
+
+        def on_finish(r):
+            if callback: callback()
+        
+        run_in_background(do_download, callback=on_finish)
+        return True
+
+_AUTHOR_RES = [
+    re.compile(r"^(.*?),\s*\d.*$", re.S),
+    re.compile(r"^(.*?);.*$", re.S),
+    ]
+
+def clean_author(au):
+    """
+    Remove cruft from Project Gutenberg author strings
+    """
+    au = au.strip()
+    for r in _AUTHOR_RES:
+        m = r.match(au)
+        if m:
+            return m.group(1)
+    return au
+
+def clean_filename(s):
+    """
+    Encode file name in filesystem charset and remove illegal characters
+    """
+    s = unicode(s).encode(sys.getfilesystemencoding(), 'replace')
+    # cleanup for VFAT and others
+    s = re.sub(r'[\x00-\x1f"\*\\/:<>?|]', '', s)
+    return s
